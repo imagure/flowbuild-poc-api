263190ad5b5f31f3317714809b4fa88c5590b792
imagure <ricardo.imagure@gmail.com>
2023-02-10 15:14:04 -0300

    feat: adds RS256 validation logic

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,31 +1,64 @@
-import jwt from 'jsonwebtoken'
-import { FastifyReply, FastifyRequest } from "fastify"
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { FastifyReply } from "fastify"
 import { envs } from '../configs/env'
 import { IActorRequest, Actor } from '../types'
+import { getClient } from './jwksClients'
 
 export const verifyJWT = async (request: IActorRequest, reply: FastifyReply, done: Function) => {
     const { headers } = request
     const { authorization } = headers as  { authorization: string } 
     
     if(authorization) {
+        let token, alg, kid, iss
         try {
-            const [,token] = authorization.split(' ')
-            const [enc_headers, enc_payload] = token.split('.')
+            ([,token] = authorization.split(' '));
+            const [enc_headers, enc_payload] = token.split('.');
 
-            const { alg } = JSON.parse(Buffer.from(enc_headers, 'base64').toString())
-            
-            if(alg === 'HS256') {
-                const payload = jwt.verify(token, envs.JWT_SECRET)
-                request.actor = payload as Actor
-                return
-            }
-            
-            //You may add any other auth logic here
-            
-        } catch(e) {
+            ({ alg, kid } = JSON.parse(Buffer.from(enc_headers, 'base64').toString()));
+            ({ iss } = JSON.parse(Buffer.from(enc_payload, 'base64').toString()));
+        } catch(err) {
             reply.code(401)
-            throw new Error('INVALID TOKEN')
+            throw new Error('INVALID TOKEN FORMAT')
         }
+        
+        if(alg === 'HS256') {
+            try {
+                const payload = jwt.verify(token, envs.JWT_SECRET) as JwtPayload
+                request.actor = { ...payload, id: payload.actor_id } as Actor
+                return
+            } catch(err) {
+                reply.code(401)
+                throw new Error(`${err}`)
+            }
+        } else
+
+
+        if(alg === 'RS256' && iss) {
+            try {
+                const key = await getClient(iss).getSigningKey(kid)
+                const publicKey = key.getPublicKey()
+                const payload = jwt.verify(token, publicKey, { 
+                    algorithms: ['RS256'],
+                }) as JwtPayload
+                
+                const {
+                    actor_id,
+                    resource_access: {
+                        'flowbuild-api': {
+                            roles
+                        }
+                    }
+                } = payload
+                request.actor = { id: actor_id, roles } as Actor
+                return
+            } catch(err) {
+                reply.code(401)
+                throw new Error(`${err}`)
+            }
+        }
+        
+        reply.code(401)
+        throw new Error('INVALID TOKEN ALGORITHM OR TOKEN FORMAT')
     }
 
     reply.code(401)
---
+++ src/auth/jwksClients.ts
@@ -0,0 +1,19 @@
+import JwksRsa from 'jwks-rsa'
+import jwksClient from 'jwks-rsa'
+import { auth } from '../configs/env'
+
+const getClient = (clientIss: string) => {
+    const authorizedIssuers = auth['RS-256-ISSUERS']
+    const clients = {} as {[key: string]: JwksRsa.JwksClient}
+    for (let [iss, certs] of Object.entries(authorizedIssuers)) {
+        clients[iss] = jwksClient({
+            jwksUri: `${certs}`,
+            cache: true
+        })
+    }
+    return clients[clientIss]
+}
+
+export {
+    getClient
+}
