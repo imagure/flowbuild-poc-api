e839d2776899417e2556af8f7944a000d3f65518
imagure <ricardo.imagure@gmail.com>
2023-02-13 10:33:28 -0300

    chore: husky test

--- src/utils/get.ts
+++ src/utils/get.ts
@@ -4,8 +4,7 @@ const getPropByPath = (
   path: string | Array<any>
 ): any => {
   const _path = Array.isArray(path) ? path : path.split('.')
-  if (source && _path.length)
-    return getPropByPath(source[_path.shift()], _path)
+  if (source && _path.length) return getPropByPath(source[_path.shift()], _path)
   return source
 }
 
